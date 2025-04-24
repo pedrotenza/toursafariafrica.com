aee08d0b6b28547c20afe9863df8fc340d8094f4
pedrotenza <pedro.tenza@hotmail.com>
2025-04-24 12:05:29 +0200

    Successfully integrated local hero video functionality

--- root/configuration/urls.py
+++ root/configuration/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from django.contrib import admin
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
@@ -11,4 +10,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
